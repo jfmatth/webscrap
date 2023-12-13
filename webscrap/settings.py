9ea6691de9ce8c441c2d24db4e77285f20a72f9b
John Matthew <john@compunique.com>
2023-12-13 13:42:25 -0800

    adjust to use DB settings

--- webscrap/settings.py
+++ webscrap/settings.py
@@ -71,17 +71,26 @@ WSGI_APPLICATION = 'webscrap.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-if not os.getenv("DATABASE_URL", None) is None:
-    DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# if not os.getenv("DATABASE_URL", None) is None:
+#     DATABASES = {
+#         "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///' + (BASE_DIR / 'db.sqlite3').as_posix(),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
